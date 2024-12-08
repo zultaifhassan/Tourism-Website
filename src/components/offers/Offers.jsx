d3f4e655f6608ee3e010c7080d85bf6b5f477501
Zultaif Hassan <zultaifhassan110@gmail.com>
2024-12-08 19:38:08 +0500

    packages

--- src/components/offers/Offers.jsx
+++ src/components/offers/Offers.jsx
@@ -11,13 +11,31 @@ const Offers = () => {
         </div>
         <div className="packages-grid-line">
           <div className="package-item">
-            <h3>Package 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. hghghfdfdf</p>
+            <img src="/Images/packages/Fm.jpg" />
+            <div className="price-duration">
+              <div className="price-days">
+                <span>Fairy Meadows, Naran, Kaghan, Hunza, Skardu</span>
+              </div>
+              <div className="days-persons">
+                <span>Per Person</span>
+                <h3>$500</h3>
+              </div>
+            </div>
+            <div className="calender-time">
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              hghghfdfdf
+            </p>
             <button>Book Now</button>
+            </div>
           </div>
           <div className="package-item">
             <h3>Package 2</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. ihkkhkjhkkjkhkjhkhjkjhkkjhkhkhjkhkhkhjkhkjhkhkjhkjhk</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              ihkkhkjhkkjkhkjhkhjkjhkkjhkhkhjkhkhkhjkhkjhkhkjhkjhk
+            </p>
             <button>Book Now</button>
           </div>
           <div className="package-item">
