9222f27b61cd56653660f1fc6563449b225ec85b
Zultaif Hassan <zultaifhassan110@gmail.com>
2024-12-09 18:17:33 +0500

    new destination

--- src/components/offers/Offers.jsx
+++ src/components/offers/Offers.jsx
@@ -22,31 +22,83 @@ const Offers = () => {
               </div>
             </div>
             <div className="calender-time">
-
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              hghghfdfdf
-            </p>
-            <button>Book Now</button>
+              <button>Book Now</button>
             </div>
           </div>
           <div className="package-item">
-            <h3>Package 2</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              ihkkhkjhkkjkhkjhkhjkjhkkjhkhkhjkhkhkhjkhkjhkhkjhkjhk
-            </p>
-            <button>Book Now</button>
+            <img src="/Images/packages/Fm.jpg" />
+            <div className="price-duration">
+              <div className="price-days">
+                <span>Fairy Meadows, Naran, Kaghan, Hunza, Skardu</span>
+              </div>
+              <div className="days-persons">
+                <span>Per Person</span>
+                <h3>$500</h3>
+              </div>
+            </div>
+            <div className="calender-time">
+              <button>Book Now</button>
+            </div>
           </div>
           <div className="package-item">
-            <h3>Package 3</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <button>Book Now</button>
+            <img src="/Images/packages/Fm.jpg" />
+            <div className="price-duration">
+              <div className="price-days">
+                <span>Fairy Meadows, Naran, Kaghan, Hunza, Skardu</span>
+              </div>
+              <div className="days-persons">
+                <span>Per Person</span>
+                <h3>$500</h3>
+              </div>
+            </div>
+            <div className="calender-time">
+              <button>Book Now</button>
+            </div>
           </div>
           <div className="package-item">
-            <h3>Package 4</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <button>Book Now</button>
+            <img src="/Images/packages/Fm.jpg" />
+            <div className="price-duration">
+              <div className="price-days">
+                <span>Fairy Meadows, Naran, Kaghan, Hunza, Skardu</span>
+              </div>
+              <div className="days-persons">
+                <span>Per Person</span>
+                <h3>$500</h3>
+              </div>
+            </div>
+            <div className="calender-time">
+              <button>Book Now</button>
+            </div>
+          </div>
+          <div className="package-item">
+            <img src="/Images/packages/Fm.jpg" />
+            <div className="price-duration">
+              <div className="price-days">
+                <span>Fairy Meadows, Naran, Kaghan, Hunza, Skardu</span>
+              </div>
+              <div className="days-persons">
+                <span>Per Person</span>
+                <h3>$500</h3>
+              </div>
+            </div>
+            <div className="calender-time">
+              <button>Book Now</button>
+            </div>
+          </div>
+          <div className="package-item">
+            <img src="/Images/packages/Fm.jpg" />
+            <div className="price-duration">
+              <div className="price-days">
+                <span>Fairy Meadows, Naran, Kaghan, Hunza, Skardu</span>
+              </div>
+              <div className="days-persons">
+                <span>Per Person</span>
+                <h3>$500</h3>
+              </div>
+            </div>
+            <div className="calender-time">
+              <button>Book Now</button>
+            </div>
           </div>
         </div>
       </div>
