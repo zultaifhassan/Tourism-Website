ee9d3e29404199bccdc55749dd0efdb8b487c4ee
Zultaif Hassan <zultaifhassan110@gmail.com>
2024-12-18 13:51:21 +0500

    popup

--- src/components/Transport/Transport.jsx
+++ src/components/Transport/Transport.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./tranport.css";
 import { IoCheckmark } from "react-icons/io5";
+import { RxCross2 } from "react-icons/rx";
+import { MdAirlineSeatReclineNormal } from "react-icons/md";
+import { TfiClose } from "react-icons/tfi";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 const Transport = () => {
   return (
@@ -19,7 +24,14 @@ const Transport = () => {
               <h2>Prado</h2>
               <div className="daily-rates">
                 <h4>Per Day</h4>
-                <h5>(15000-20000)</h5>
+                <h5>(Rs 15000-20000)</h5>
+              </div>
+              <div className="vehicle-capacity">
+                <h4>
+                  <MdAirlineSeatReclineNormal fontSize={28} color="green" />{" "}
+                  Capacity{" "}
+                </h4>
+                <h5>3-4 Persons</h5>
               </div>
               <div className="vehicle-including">
                 <div className="transport-includes">
@@ -42,11 +54,7 @@ const Transport = () => {
                 </div>
                 <div className="transport-includes">
                   <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
+                    <RxCross2 />
                     <h4>Driver</h4>
                   </span>
                   <span>
@@ -59,313 +67,19 @@ const Transport = () => {
                   </span>
                 </div>
               </div>
-              <button>Book Now</button>
-            </div>
-          </div>
-          <div className="transport-boxes">
-            <div className="transport-image">
-              <img src="/Images/Transport/Prado.jpg" alt="" />
-            </div>
-            <div className="transport-details">
-              <h2>Prado</h2>
-              <div className="daily-rates">
-                <h4>Per Day</h4>
-                <h5>(15000-20000)</h5>
-              </div>
-              <div className="vehicle-including">
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-              </div>
-              <button>Book Now</button>
-            </div>
-          </div>
-          <div className="transport-boxes">
-            <div className="transport-image">
-              <img src="/Images/Transport/Prado.jpg" alt="" />
-            </div>
-            <div className="transport-details">
-              <h2>Prado</h2>
-              <div className="daily-rates">
-                <h4>Per Day</h4>
-                <h5>(15000-20000)</h5>
-              </div>
-              <div className="vehicle-including">
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-              </div>
-              <button>Book Now</button>
-            </div>
-          </div>
-          <div className="transport-boxes">
-            <div className="transport-image">
-              <img src="/Images/Transport/Prado.jpg" alt="" />
-            </div>
-            <div className="transport-details">
-              <h2>Prado</h2>
-              <div className="daily-rates">
-                <h4>Per Day</h4>
-                <h5>(15000-20000)</h5>
-              </div>
-              <div className="vehicle-including">
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-              </div>
-              <button>Book Now</button>
-            </div>
-          </div>
-          <div className="transport-boxes">
-            <div className="transport-image">
-              <img src="/Images/Transport/Prado.jpg" alt="" />
-            </div>
-            <div className="transport-details">
-              <h2>Prado</h2>
-              <div className="daily-rates">
-                <h4>Per Day</h4>
-                <h5>(15000-20000)</h5>
-              </div>
-              <div className="vehicle-including">
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={23}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-              </div>
-              <button>Book Now</button>
-            </div>
-          </div>
-          <div className="transport-boxes">
-            <div className="transport-image">
-              <img src="/Images/Transport/Prado.jpg" alt="" />
-            </div>
-            <div className="transport-details">
-              <h2>Prado</h2>
-              <div className="daily-rates">
-                <h4>Per Day</h4>
-                <h5>(15000-20000)</h5>
-              </div>
-              <div className="vehicle-including">
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={23}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-              </div>
-              <button>Book Now</button>
-            </div>
-          </div>
-          <div className="transport-boxes">
-            <div className="transport-image">
-              <img src="/Images/Transport/Prado.jpg" alt="" />
-            </div>
-            <div className="transport-details">
-              <h2>Prado</h2>
-              <div className="daily-rates">
-                <h4>Per Day</h4>
-                <h5>(15000-20000)</h5>
-              </div>
-              <div className="vehicle-including">
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={23}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-                <div className="transport-includes">
-                  <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
-                    <h4>Driver</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Fuel</h4>
-                  </span>
-                  <span>
-                    <IoCheckmark fontSize={20} />
-                    <h4>Driver</h4>
-                  </span>
-                </div>
-              </div>
-              <button>Book Now</button>
+              <Popup
+                trigger={<button>Book Now</button>}
+                modal
+                nested>
+                {(close) => (
+                  <div className="popu-booking-form">
+                    <div className="content">Book Transport You Want</div>
+                    <div className="popup-close-botton">
+                      <button onClick={() => close()}>Close <TfiClose /></button>
+                    </div>
+                  </div>
+                )}
+              </Popup>
             </div>
           </div>
         </div>
