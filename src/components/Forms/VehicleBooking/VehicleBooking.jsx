bf1e04a34a557b548ac840cfce6baef48ef729c9
Zultaif Hassan <zultaifhassan110@gmail.com>
2024-12-21 00:15:00 +0500

    popuup

--- src/components/Forms/VehicleBooking/VehicleBooking.jsx
+++ src/components/Forms/VehicleBooking/VehicleBooking.jsx
@@ -1,32 +1,68 @@
 import React from "react";
 import "./vehiclebooking.css";
+import { LiaUserSolid } from "react-icons/lia";
+import { CiMail } from "react-icons/ci";
+import { PiWhatsappLogoThin } from "react-icons/pi";
+import { IoCarSportOutline } from "react-icons/io5";
+import { TbBrandDaysCounter } from "react-icons/tb";
 
 const VehicleBooking = () => {
   return (
     <div className="vehicle-booking-form">
       <form>
         <span>
-          <input type="text" placeholder="Full Name" />
-          <input type="text" placeholder="Fathers Name" />
+          <div className="icon-users-name">
+            <LiaUserSolid />
+            <input type="text" placeholder="Full Name" />
+          </div>
+          <div className="icon-users-name">
+            <LiaUserSolid />
+            <input type="text" placeholder="Fathers Name" />
+          </div>
         </span>
         <span>
-          <input type="email" placeholder="Email" />
-          <input type="number" placeholder="Phone Number" />
+          <div className="icon-users-name">
+            <CiMail />
+            <input type="text" placeholder="Email" />
+          </div>
+          <div className="icon-users-name">
+            <PiWhatsappLogoThin />
+            <input type="number" placeholder="Phone Number" />
+          </div>
         </span>
         <span>
-          <select name="cars" id="cars">
-            <option value="Select Model">Select Model</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
-          </select>
-          <input type="number" placeholder="Total Days" />
+          <div className="icon-users-name">
+            <IoCarSportOutline fontSize={25} />
+            <select name="cars" id="cars">
+              <option value="Select Model" selected disabled>
+                Select Model
+              </option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </div>
+
+          <div className="icon-users-name">
+            <TbBrandDaysCounter />
+            <input type="number" placeholder="Total Days" />
+          </div>
         </span>
         <div className="select-dates-from">
           <h1>Select Dates</h1>
           <span>
-            <input type="date" placeholder="From" />
-            <input type="date" placeholder="To" />
+            <div className="selected-date-duration">
+              <p>From</p>
+              <div className="icon-users-name">
+                <input type="date" placeholder="From" />
+              </div>
+            </div>
+            <div className="selected-date-duration">
+              <p>To</p>
+              <div className="icon-users-name">
+                <input type="date" placeholder="To" />
+              </div>
+            </div>
           </span>
         </div>
         <button>Submit</button>
--- src/components/Transport/Transport.jsx
+++ src/components/Transport/Transport.jsx
@@ -29,41 +29,37 @@ const Transport = () => {
               </div>
               <div className="vehicle-capacity">
                 <h4>
-                  <MdAirlineSeatReclineNormal fontSize={28} color="green" />{" "}
-                  Capacity{" "}
+                  <MdAirlineSeatReclineNormal fontSize={25} color="green" />
+                  Capacity
                 </h4>
                 <h5>3-4 Persons</h5>
               </div>
               <div className="vehicle-including">
                 <div className="transport-includes">
                   <span>
-                    <IoCheckmark
-                      fontSize={25}
-                      color="green"
-                      fontWeight={"bold"}
-                    />
+                    <IoCheckmark fontSize={22} color="green" />
                     <h4>Driver</h4>
                   </span>
                   <span>
-                    <IoCheckmark fontSize={20} />
+                    <IoCheckmark fontSize={22} color="green" />
                     <h4>Fuel</h4>
                   </span>
                   <span>
-                    <IoCheckmark fontSize={20} />
+                    <IoCheckmark fontSize={22} color="green" />
                     <h4>Driver</h4>
                   </span>
                 </div>
                 <div className="transport-includes">
                   <span>
-                    <RxCross2 />
+                    <RxCross2 fontSize={21} color="Red" />
                     <h4>Driver</h4>
                   </span>
                   <span>
-                    <IoCheckmark fontSize={20} />
+                    <RxCross2 fontSize={21} color="Red" />
                     <h4>Fuel</h4>
                   </span>
                   <span>
-                    <IoCheckmark fontSize={20} />
+                    <RxCross2 fontSize={21} color="Red" />
                     <h4>Driver</h4>
                   </span>
                 </div>
@@ -74,9 +70,146 @@ const Transport = () => {
                     <div className="close-button-name">
                       <div className="content">Book Transport You Want</div>
                       <div className="popup-close-botton">
-                        <button onClick={() => close()}>
-                          Close <TfiClose />
-                        </button>
+                        <TfiClose
+                          fontSize={25}
+                          color="red"
+                          cursor="Pointer"
+                          onClick={() => close()}
+                        />
+                      </div>
+                    </div>
+                    <VehicleBooking />
+                  </div>
+                )}
+              </Popup>
+            </div>
+          </div>
+          <div className="transport-boxes">
+            <div className="transport-image">
+              <img src="/Images/Transport/Prado1.jpg" alt="" />
+            </div>
+            <div className="transport-details">
+              <h2>Prado</h2>
+              <div className="daily-rates">
+                <h4>Per Day</h4>
+                <h5>(Rs 15000-20000)</h5>
+              </div>
+              <div className="vehicle-capacity">
+                <h4>
+                  <MdAirlineSeatReclineNormal fontSize={25} color="green" />
+                  Capacity
+                </h4>
+                <h5>3-4 Persons</h5>
+              </div>
+              <div className="vehicle-including">
+                <div className="transport-includes">
+                  <span>
+                    <IoCheckmark fontSize={22} color="green" />
+                    <h4>Driver</h4>
+                  </span>
+                  <span>
+                    <IoCheckmark fontSize={22} color="green" />
+                    <h4>Fuel</h4>
+                  </span>
+                  <span>
+                    <IoCheckmark fontSize={22} color="green" />
+                    <h4>Driver</h4>
+                  </span>
+                </div>
+                <div className="transport-includes">
+                  <span>
+                    <RxCross2 fontSize={21} color="Red" />
+                    <h4>Driver</h4>
+                  </span>
+                  <span>
+                    <RxCross2 fontSize={21} color="Red" />
+                    <h4>Fuel</h4>
+                  </span>
+                  <span>
+                    <RxCross2 fontSize={21} color="Red" />
+                    <h4>Driver</h4>
+                  </span>
+                </div>
+              </div>
+              <Popup trigger={<button>Book Now</button>} modal nested>
+                {(close) => (
+                  <div className="popu-booking-form">
+                    <div className="close-button-name">
+                      <div className="content">Book Transport You Want</div>
+                      <div className="popup-close-botton">
+                        <TfiClose
+                          fontSize={25}
+                          color="red"
+                          cursor="Pointer"
+                          onClick={() => close()}
+                        />
+                      </div>
+                    </div>
+                    <VehicleBooking />
+                  </div>
+                )}
+              </Popup>
+            </div>
+          </div>
+          <div className="transport-boxes">
+            <div className="transport-image">
+              <img src="/Images/Transport/jeep1.jpg" alt="" />
+            </div>
+            <div className="transport-details">
+              <h2>Prado</h2>
+              <div className="daily-rates">
+                <h4>Per Day</h4>
+                <h5>(Rs 15000-20000)</h5>
+              </div>
+              <div className="vehicle-capacity">
+                <h4>
+                  <MdAirlineSeatReclineNormal fontSize={25} color="green" />
+                  Capacity
+                </h4>
+                <h5>3-4 Persons</h5>
+              </div>
+              <div className="vehicle-including">
+                <div className="transport-includes">
+                  <span>
+                    <IoCheckmark fontSize={22} color="green" />
+                    <h4>Driver</h4>
+                  </span>
+                  <span>
+                    <IoCheckmark fontSize={22} color="green" />
+                    <h4>Fuel</h4>
+                  </span>
+                  <span>
+                    <IoCheckmark fontSize={22} color="green" />
+                    <h4>Driver</h4>
+                  </span>
+                </div>
+                <div className="transport-includes">
+                  <span>
+                    <RxCross2 fontSize={21} color="Red" />
+                    <h4>Driver</h4>
+                  </span>
+                  <span>
+                    <RxCross2 fontSize={21} color="Red" />
+                    <h4>Fuel</h4>
+                  </span>
+                  <span>
+                    <RxCross2 fontSize={21} color="Red" />
+                    <h4>Driver</h4>
+                  </span>
+                </div>
+              </div>
+              <Popup trigger={<button>Book Now</button>} modal nested>
+                {(close) => (
+                  <div className="popu-booking-form">
+                    <div className="close-button-name">
+                      <div className="content">Book Transport You Want</div>
+                      <div className="popup-close-botton">
+                        <TfiClose
+                          fontSize={25}
+                          color="red"
+                          cursor="Pointer"
+                          onClick={() => close()}
+                        />
                       </div>
                     </div>
                     <VehicleBooking />
