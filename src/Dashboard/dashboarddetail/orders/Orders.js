cd8acf2c727c58748f9e143ca47dbccb0fdc928b
Zultaif Hassan <zultaifhassan110@gmail.com>
2025-03-27 15:00:08 +0500

    tables added

--- src/Dashboard/dashboarddetail/orders/Orders.js
+++ src/Dashboard/dashboarddetail/orders/Orders.js
@@ -7,17 +7,32 @@ const Orders = () => {
       <h1>Orders List</h1>
       <div className="order-table">
           <tr>
-            <th>Name</th>
-            <th>Date</th>
-            <th>Status</th>
-            <th>Mobile No</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Action</th>
+            <th className="name-attribute">Name</th>
+            <th className="date-attribute">Date</th>
+            <th className="toald-attribute">Total.D</th>
+            <th className="status-attribute">Status</th>
+            <th className="number-attribute">Mobile No</th>
+            <th className="email-attribute">Email</th>
+            <th className="address-attribute">Address</th>
+            <th className="action-attribute">Action</th>
           </tr>
           <tr>
             <td>Zultaif Hassan</td>
             <td>10-04-2025</td>
+            <td>8 Days</td>
+            <td className="status-news"><span>Pending</span></td>
+            <td>03495419344</td>
+            <td>zultaifhassan110@gmail.com</td>
+            <td>Bunji House Near Post Office</td>
+            <td className="action-icons-editdel">
+              <img src="/Images/dashboard/edit.png" alt="img" />
+              <img src="/Images/dashboard/delete.png" alt="img"></img>
+            </td>
+          </tr>
+          <tr>
+            <td>Zultaif Hassan</td>
+            <td>10-04-2025</td>
+            <td>8 Days</td>
             <td className="status-news"><span>Pending</span></td>
             <td>03495419344</td>
             <td>zultaifhassan110@gmail.com</td>
