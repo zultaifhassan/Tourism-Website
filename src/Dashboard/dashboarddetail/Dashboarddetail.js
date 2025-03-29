343afaf53d788559122885d87c42421d0443820d
Zultaif Hassan <zultaifhassan110@gmail.com>
2025-03-30 02:28:26 +0500

    little

--- src/Dashboard/dashboarddetail/Dashboarddetail.js
+++ src/Dashboard/dashboarddetail/Dashboarddetail.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./dashboarddetail.css";
 
 const Dashboarddetail = () => {
   return (
-    <div>Dashboarddetail</div>
-  )
-}
+    <div className="dashborad-main-page">
+      <div className="dashboard-wellcomenote">
+        <h2>Wellcome To Karakoram Safari</h2>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboarddetail
+export default Dashboarddetail;
